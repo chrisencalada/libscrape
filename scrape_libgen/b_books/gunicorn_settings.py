109fb5f4983be5f55f1e1412b57c4e8b85494102
chrisencalada <cencalada1994@gmail.com>
2019-10-21 00:35:03 +0000

    clean up

--- scrape_libgen/b_books/gunicorn_settings.py
+++ scrape_libgen/b_books/gunicorn_settings.py
@@ -2,7 +2,7 @@ worker_class='gevent'
 timeout=5000
 accesslog = '/home/coli/libscrape/libscrape/scrape_libgen/b_books/gunicorn_access_log'
 errorlog = '/home/coli/libscrape/libscrape/scrape_libgen/b_books/gunicorn_error_log'
-
+loglevel='debug'
 
 
 
